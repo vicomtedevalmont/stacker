d3c2bc071148be891f34e43ae5e4b468c367ba5e
Sébastien de Valmont <87944942+vicomtedevalmont@users.noreply.github.com>
2021-11-29 18:39:15 +0300

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <b-container fluid class="text-center p-0 m-0">
+      <Navbar />
+      <b-jumbotron
+        header="Stacks"
+        header-tag="h2"
+        fluid
+        class="m-0 p-0 p-3"
+        header-level="4"
+      >
+      </b-jumbotron>
+    </b-container>
+    <b-container>
+      <div class="mt-3">
+        <b-card-group deck>
+          <b-card header="Default" class="text-center">
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </b-card-text>
+          </b-card>
+          <b-card header="Default" class="text-center">
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </b-card-text>
+          </b-card>
+          <b-card header="Default" class="text-center">
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "@/utils/vue-imports";
+import Navbar from "@/components/Navbar.vue";
+
+@Component({
+  name: "Home",
+  components: {
+    Navbar,
+  },
+})
+export default class Home extends Vue {
+  images = [
+    ["/img/pwa.png", "/img/pwa.png", "/img/pwa.png"],
+    ["/img/pwa.png", "/img/pwa.png"],
+  ];
+}
+</script>
